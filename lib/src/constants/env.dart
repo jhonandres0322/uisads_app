6ccd59c42a353fabc1490e616f34d4a2e6e0e534
Jhon <andresparrarodriguez@hotmail.com>
2022-06-10 17:52:00 -0500

    cambios en la conexion del backend

--- lib/src/constants/env.dart
+++ lib/src/constants/env.dart
@@ -1,9 +1,9 @@
 class Env {
-  final String _endpointDev = 'http://192.168.1.6:5000/api';
+  final String _endpointDev = 'http://192.168.0.103:5000/api';
   final String _endpointPrd = '';
 
-  String getEndpoint( String type ) {
-    if (type.contains('dev')) {
+  String getEndpoint(String type) {
+    if (type == 'dev') {
       return _endpointDev;
     } else {
       return _endpointPrd;
--- lib/src/utils/http_handler.dart
+++ lib/src/utils/http_handler.dart
@@ -1,75 +1,70 @@
 import 'dart:async';
 import 'dart:convert';
 import 'dart:developer';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:uisads_app/src/constants/env.dart';
-import 'package:uisads_app/src/widgets/alert_error.dart';
-
-// ignore: todo
-// TODO: Cambiar por el token de preferences shared
-String token = 'este es el token';
 
 class HttpHandler {
-  // ignore: non_constant_identifier_names
   final String _baseUrl = Env().getEndpoint('dev');
-  Map<String, String> headers = {
+  final String token = '';
+
+  final Map<String, String> _headers = {
     'Content-type': 'application/x-www-form-urlencoded',
     'Accept': 'application/json',
     'charset': 'utf-8'
   };
 
-  Map<String, String> _addTokenHeader(
-      String token, Map<String, String> headers) {
+  String _getEndpoint(String endpoint) => _baseUrl + endpoint;
+
+  Map<String, String> _getHeaders() {
     if (token.isNotEmpty) {
       Map<String, String> accessToken = {'access-token': token};
-      headers.addAll(accessToken);
-      debugPrint(" headers --> $headers ");
-      return headers;
+      _headers.addAll(accessToken);
+      log("headers with token --> $_headers");
+      return _headers;
     }
-    return headers;
+    log("headers without token --> $_headers");
+    return _headers;
   }
 
-  Future<dynamic> getGet(String endpoint) async {
-    Map<String, String> getHeaders = _addTokenHeader(token, headers);
+  // ignore: unused_element
+  Future<Map<String, dynamic>> getGet(String endpoint) async {
+    Map<String, String> getHeaders = _getHeaders();
+    String url = _getEndpoint(endpoint);
+    final resp = await http.get(Uri.parse(url), headers: getHeaders);
+    log("response --> $resp");
+    return json.decode(resp.body);
+  }
+
+  // ignore: unused_element
+  Future<Map<String, dynamic>> getPost(
+      String endpoint, Map<String, dynamic> request) async {
+    log("Entrando al metodo post");
+    Map<String, String> getHeaders = _getHeaders();
+    String url = _getEndpoint(endpoint);
+    log("endpoint --> $url");
+    final resp = await http.post(Uri.parse(url), headers: getHeaders, body: request);
+    log("response --> $resp");
+    return json.decode(resp.body);
+  }
+
+  // ignore: unused_element
+  Future<Map<String, dynamic>> getPut(
+      String endpoint, Map<String, dynamic> request) async {
+    Map<String, String> getHeaders = _getHeaders();
+    String url = _getEndpoint(endpoint);
     final resp =
-        await http.get(Uri.parse('$_baseUrl$endpoint'), headers: getHeaders);
-    int statusCode = resp.statusCode;
-    if (statusCode < 200 || statusCode > 399) {
-      throw Exception(statusCode);
-    }
+        await http.put(Uri.parse(url), headers: getHeaders, body: request);
+    log("response --> $resp");
     return json.decode(resp.body);
   }
 
-  Future<dynamic> getPost(String endpoint, Map<String, dynamic> request) async {
-    Map<String, String> postHeaders = _addTokenHeader(token, headers);
-    final resp = await http.post(Uri.parse('$_baseUrl$endpoint'),
-        headers: postHeaders, body: request);
-    final decoded = json.decode( resp.body );
-    log('decoded --> $decoded');
-    return decoded;
-  }
-
-  Future<dynamic> getPut(String endpoint, Map<String, dynamic> request) async {
-    Map<String, String> putHeaders = _addTokenHeader(token, headers);
-    final resp = await http.put(Uri.parse('$_baseUrl$endpoint'),
-        headers: putHeaders, body: request);
-    int statusCode = resp.statusCode;
-    if (statusCode < 200 || statusCode > 399) {
-      throw Exception(statusCode);
-    }
-    return json.decode(resp.body);
-  }
-
-  Future<dynamic> getDelete(String endpoint) async {
-    Map<String, String> deleteHeaders = _addTokenHeader(token, headers);
-    final resp = await http.delete(Uri.parse('$_baseUrl$endpoint'),
-        headers: deleteHeaders);
-    int statusCode = resp.statusCode;
-    if (statusCode < 200 || statusCode > 399) {
-      throw Exception(statusCode);
-    }
+  // ignore: unused_element
+  Future<Map<String, dynamic>> getDelete(String endpoint) async {
+    Map<String, String> getHeaders = _getHeaders();
+    String url = _getEndpoint(endpoint);
+    final resp = await http.delete(Uri.parse(url), headers: getHeaders);
+    log("response --> $resp");
     return json.decode(resp.body);
   }
 }
