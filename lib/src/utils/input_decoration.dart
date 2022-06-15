98ecb77836e13839a1b16faa271cc6b11149f5cb
Jhon <andresparrarodriguez@hotmail.com>
2022-06-14 23:32:55 -0500

    cambios en la estructura del input_custom, se agrego clase para la decoracion

---
+++ lib/src/utils/input_decoration.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:uisads_app/src/constants/colors.dart';
+
+InputDecoration decorationInputCustom(IconData icon, String hintText) {
+  const double widthBorderInput = 1.5;
+  const double borderRadiusInput = 10.0;
+  return InputDecoration(
+    contentPadding: const EdgeInsets.symmetric(vertical: 2.0, horizontal: 2.0),
+    border: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(borderRadiusInput)
+    ),
+    hintText: hintText,
+    hintStyle: const TextStyle(
+      fontSize: 12.0, 
+      color: AppColors.subtitles
+    ),
+    prefixIcon: Icon(icon, color: AppColors.subtitles),
+    prefixIconColor: AppColors.primary,
+    enabledBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(borderRadiusInput),
+      borderSide: const BorderSide(
+        color: AppColors.primary, width: widthBorderInput
+      )
+    ),
+    focusedBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(borderRadiusInput),
+      borderSide: const BorderSide(
+      color: AppColors.primary, width: widthBorderInput)
+    )
+  );
+}
--- lib/src/widgets/input_custom.dart
+++ lib/src/widgets/input_custom.dart
@@ -1,30 +1,19 @@
-import 'dart:developer';
 
 import 'package:flutter/material.dart';
 import 'package:uisads_app/src/constants/colors.dart';
 
+
 // ignore: must_be_immutable
 class InputCustom extends StatelessWidget {
-  final String hintText;
   final String labelText;
-  final bool obscureText;
-  final TextInputType keyboardType;
-  final IconData icon;
+  final Widget input;
 
-  String value = '';
+  const InputCustom({
+    Key? key,
+    required this.labelText,
+    required this.input
+  }) : super(key: key);
 
-  InputCustom(
-      {Key? key,
-      required this.hintText,
-      this.labelText = '',
-      this.obscureText = false,
-      required this.keyboardType,
-      required this.icon,
-      required this.value})
-      : super(key: key);
-
-  final double widthBorderInput = 1.5;
-  final double borderRadiusInput = 10.0;
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
@@ -33,58 +22,27 @@ class InputCustom extends StatelessWidget {
       child: Column(
         children: [
           Padding(
-            padding: EdgeInsets.only(left: size.width * 0.01),
+            padding: EdgeInsets.only(
+              left: size.width * 0.01,
+              top: size.height * 0.025
+            ),
             child: Align(
               alignment: Alignment.topLeft,
               child: Text(
                 labelText,
                 style: const TextStyle(
-                  color: AppColors.subtitles,
-                  fontSize: 14, 
-                  fontWeight: FontWeight.w400, 
-                  fontFamily: 'Roboto'
-                ),
+                    color: AppColors.subtitles,
+                    fontSize: 11,
+                    fontWeight: FontWeight.w400,
+                    fontFamily: 'Roboto'),
               ),
             ),
           ),
           SizedBox(
             height: size.height * 0.01,
           ),
+          input
           // ignore: sized_box_for_whitespace
-          TextFormField(
-            autofocus: false,
-            obscureText: obscureText,
-            keyboardType: keyboardType,
-            initialValue: '',
-            onChanged: (value) {
-              this.value = value;
-              log("onchange value --> $value");
-            },
-            validator: (value) {
-              if (value == null) return "Este campo es requerido";
-              return value.length < 3 ? 'Minimo de 3 letras' : null;
-            },
-            autovalidateMode: AutovalidateMode.onUserInteraction,
-            decoration: InputDecoration(
-              contentPadding: const EdgeInsets.symmetric(vertical: 2.0, horizontal: 2.0),
-              border: OutlineInputBorder(borderRadius: BorderRadius.circular(borderRadiusInput)),
-                prefixIcon: Icon(icon, color: AppColors.subtitles),
-                prefixIconColor: AppColors.primary,
-                enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(borderRadiusInput),
-                    borderSide: BorderSide(
-                        color: AppColors.primary, width: widthBorderInput)),
-                focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(borderRadiusInput),
-                    borderSide: BorderSide(
-                        color: AppColors.primary, width: widthBorderInput)),
-                hintText: hintText,
-                hintStyle: const TextStyle(
-                    fontSize: 12.0, 
-                    color: AppColors.subtitles
-                )
-            ),
-          ),
         ],
       ),
     );
