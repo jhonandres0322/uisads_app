24c5e833ee58b1127a7d9c5e0949da004758b9e3
Jhon Andres Parra Rodriguez <andresparrarodriguez@hotmail.com>
2022-08-06 07:17:31 -0500

    Cambios en la edición del perfil
    Se añadieron cambios para traer la información del perfil en la vista de edición, ademas se modificaron los metodos para eliminar los datos que se editaron

--- lib/src/providers/edit_profile_provider.dart
+++ lib/src/providers/edit_profile_provider.dart
@@ -6,76 +6,33 @@ import '../models/upload.dart';
 
 class EditProfileProvider with ChangeNotifier{
 
-  String _name = '';
-  String _cellphone = '';
-  String _email = '';
-  String _city = '';
-  String _description = '';
-  String _imageProfile = '';
-  Map<String,dynamic> _image = {} ;
-  final _formKey = GlobalKey<FormState>();
+  String name                  = '';
+  String cellphone             = '';
+  String email                 = '';
+  String city                  = '';
+  String description           = '';
+  Upload image                 = Upload() ;
+  GlobalKey<FormState> formKey = GlobalKey<FormState>( );
 
-  GlobalKey<FormState> get formKey => _formKey;
 
-  String get imageProfile => _imageProfile;
-  set imageProfile( String value ) {
-    _imageProfile = value;
-    notifyListeners();
+  Profile getDataEditProfile() {
+    return Profile.fromMap({
+      "name"        : name,
+      "cellphone"   : cellphone,
+      "city"        : city,
+      "email"       : email,
+      "description" : description,
+      "image"       : image.toMap()
+    });
   }
 
-  Map<String,dynamic> get image => _image;
-  set image ( Map<String,dynamic> value ) {
-    _image = value;
-    notifyListeners();
-  }
-  String get name => _name;
-  set name( String value ) {
-    _name = value;
-    notifyListeners();
-  }
-
-  String get cellphone => _cellphone;
-  set cellphone( String value ) {
-    _cellphone = value;
-    notifyListeners();
-  }
-
-  String get email => _email;
-  set email( String value ) {
-    _email = value;
-    notifyListeners();
-  }
-
-  String get city => _city;
-  set city( String value ) {
-    _city = value;
-    notifyListeners();
-  }
-
-  String get description => _description;
-  set description( String value ) {
-    _description = value;
-    notifyListeners();
-  }
-
-  Map<String,dynamic> getData() {
-    return {
-      "name" : _name,
-      "cellphone" : _cellphone,
-      "email" : _email,
-      "city" : _city,
-      "description" : _description,
-      "image": _image
-    }; 
-  }
-
-  void clearData() {
-    _name = '';
-    _cellphone = '';
-    _email = '';
-    _city = '';
-    _imageProfile = '';
-    _image = {};
+  void clearDataEditProfile() {
+    name        = '';
+    cellphone   = '';
+    email       = '';
+    city        = '';
+    description = '';
+    image       = Upload();
   }
 
 }
